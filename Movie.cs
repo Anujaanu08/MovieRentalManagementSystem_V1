0469e471ab99d1e59c8f80fd7547313cbea0b1b7
Anuja V. <UT01463@unicomtic.com>
2024-10-11 09:51:31 +0530

    Q2

---
+++ Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRentalManagementSystem_V1
+{
+    internal class Movie
+    {
+        private string movieId;
+        private string title;
+        private string director;
+        private string rentalPrice;
+
+    }
+}
